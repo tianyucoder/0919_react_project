7f30c67c2fc74c576fd0689ab405ec1fddd66ede
tianyu5177 <zhangtianyu5177@sina.com>
2020-02-10 15:52:59 +0800

    更新

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import logo from './img/logo.png'
+import './css/login.less'
 
 export default class Login extends Component {
 	render() {
 		return (
-			<h1>
-				Login
-			</h1>
+			<div id="login">
+				<div className="header">
+					<img src={logo} alt="logo"/>
+					<h1>商品管理系统</h1>
+				</div>
+				<div className="content">
+					<h1>用户登录</h1>
+					<div>这里以后会放antd的表单</div>
+				</div>
+			</div>
 		)
 	}
 }
