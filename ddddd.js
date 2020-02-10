5d3b8c6468ec1f759034b168078a4eeb7c2f0f53
tianyu5177 <zhangtianyu5177@sina.com>
2020-02-10 17:14:17 +0800

    更新

--- ddddd.js
+++
@@ -1,8 +0,0 @@
-getFieldDecorator('username', {rules: [{ required: true, message: '请输入用户名！' }],})(
-	<Input
-		prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-		placeholder="Username"
-	/>,
-)
-
-
